a7dc5232f0b773d1daf66ec42bae2ea866e52bed
Tanner-Ray-Martin <tanner.ray.martin.123@gmail.com>
2024-07-05 15:41:50 -0500

    get requests and responses inital

--- POC/api/main.py
+++ POC/api/main.py
@@ -1,37 +1,63 @@
-# main.py
-from fastapi import FastAPI, Depends, HTTPException
-from sqlalchemy.orm import Session
-from db.database import SessionLocal, engine, Base
-from db.models import UserModel, GenModel
-from schemas.schemas import UserSchema, GenSchema
-from db import crud
-
-# Create the database tables
-Base.metadata.create_all(bind=engine)
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import HTMLResponse
+from sqlmodel import Field, Session, SQLModel, create_engine, select
+from typing import Optional, Literal
+from fastui import FastUI, AnyComponent, prebuilt_html, components as c
+from fastui.components.display import DisplayMode, DisplayLookup
+from fastui.events import GoToEvent, BackEvent
+from pydantic import BaseModel, Field as DanticField
 
 app = FastAPI()
+fast_ui = FastUI(app)
 
-# Dependency to get the database session
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+DATABASE_URL = "sqlite:///./test.db"
+engine = create_engine(DATABASE_URL)
 
-@app.put("/users/{user_id}", response_model=UserSchema)
-def update_user(user_id: int, user_update: UserSchema, db: Session = Depends(get_db)):
-    db_user = crud.update(db, user_id, user_update)
-    if db_user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    return db_user
+field_types = Literal["string", "integer", "float", "date", "datetime", "relationship", "email", "phone"]
 
-@app.get("/users/all", response_model=list[UserSchema])
-def get_all_users(db:Session = Depends(get_db)):
-    users = crud.get_all_users(db)
-    return users
+class CreationModelField(SQLModel, table=True):
+    field_name:str
+    field_type:field_types
+    access_level:int
 
-@app.post("/users/create", response_model=UserSchema)
-def create_user(user_create:UserSchema,db:Session = Depends(get_db)):
-    user = crud.create(db, user_create)
-    return user
+class CreationModel(SQLModel, table=True):
+    id: int
+    model_name:str
+    parent_names:list[str]
+    child_names:list[str]
+    basic_schema:list[CreationModelField]
+
+
+class UserModel(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str
+    email: str
+
+class User(BaseModel):
+    id: Optional[int] = DanticField(default=None, primary_key=True)
+    name: str
+    email: str
+
+SQLModel.metadata.create_all(engine)
+
+@app.get("/users/", response_model=FastUI, response_model_exclude_none=True)
+def get_all_users()->list[AnyComponent]:
+    # return a table of all users
+    ...
+
+
+@app.get("/users/{user_id}", response_model=FastUI, response_model_exclude_none=True)
+def read_user(user_id: int) -> list[AnyComponent]:
+    #get user by id and return a fastui view of the users profile
+    ...
+
+
+@app.get('/{path:path}')
+async def html_landing() -> HTMLResponse:
+    """Simple HTML page which serves the React app, comes last as it matches all paths."""
+    return HTMLResponse(prebuilt_html(title='FastUI Demo'))
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
